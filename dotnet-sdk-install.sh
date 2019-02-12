ec21ad996253ae783182cd354025e6b84a83ab8f
Y2zz <yangyunzhou@foxmail.com>
2019-02-13 01:45:24 +0800

    update script

--- dotnet-sdk-install.sh
+++ dotnet-sdk-install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sudo apt-get install curl libunwind8 gettext libicu52
-curl -sSL -o dotnet.tar.gz https://download.visualstudio.microsoft.com/download/pr/296e116b-30d7-4e1c-8238-ec8c7c4c7b79/43d6cd35d95e38675d472c56a24c3bd0/dotnet-sdk-2.2.103-linux-x64.tar.gz
+sudo apt-get install wget libunwind8 gettext libicu52
+wget -O dotnet.tar.gz https://download.visualstudio.microsoft.com/download/pr/296e116b-30d7-4e1c-8238-ec8c7c4c7b79/43d6cd35d95e38675d472c56a24c3bd0/dotnet-sdk-2.2.103-linux-x64.tar.gz
 sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
 sudo ln -s /opt/dotnet/dotnet /usr/local/bin
+sudo rm dotnet.tar.gz
